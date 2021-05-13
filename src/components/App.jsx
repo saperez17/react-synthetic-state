ed8e50d6bdbb40dce43e48f4668845c6bb060b6d
Santiago Alvarado <34078372+saperez17@users.noreply.github.com>
2021-05-12 22:32:30 -0500

    state handling - Object Use an object as the state variable.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [contact, setContact] = useState({ fName: "", lName: "", email: "" });
+
+  const handleNameChange = (event) => {
+    const { value, name } = event.target;
+    setContact((prevState) => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+      <h1>
+        Hello {contact.fName} {contact.lName}{" "}
+      </h1>
+      <p>{contact.email}</p>
+      <form onSubmit={(event) => event.preventDefault()}>
+        <input
+          onChange={handleNameChange}
+          name="fName"
+          placeholder="First Name"
+        />
+        <input
+          onChange={handleNameChange}
+          name="lName"
+          placeholder="Last Name"
+        />
+        <input onChange={handleNameChange} name="email" placeholder="Email" />
         <button>Submit</button>
       </form>
     </div>
